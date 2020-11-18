40f3449c6e0c72c14eeb4fa07d708a280117b68e
pwting1119 <71752445+pwting1119@users.noreply.github.com>
2020-11-18 14:26:54 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showArrow(ArrowNames.West)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showArrow(ArrowNames.East)
+})
+basic.forever(function () {
+	
+})
